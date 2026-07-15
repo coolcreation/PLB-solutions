57382edba897328c7afd98966ea7750428ac9804
Jeff <jeffbohn@my.cwi.edu>
2026-07-15 17:13:03 -0600

    text decoration noe for text logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import './Logo.css'
 export default function Logo() {
   return (
     <>
-     <a className="text-logo" href='https://coolcreation.github.io/PLB-solutions/'>
+     <a className="text-logo text-decoration-none" href='https://coolcreation.github.io/PLB-solutions/'>
         PLB
      </a>
     {/* <img src="/favicon.svg" alt="logo" /> */}
